b8715fa6918b16eb9cbcfd576351abf835f067af
g992987642 <992987642@qq.com>
2020-09-23 22:29:39 +0800

    [guhao]:finished exercise-4

--- src/exercise-4/components/User.js
+++ src/exercise-4/components/User.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import NotMatch from './NotMatch'
 
-const User = () => {
-  return (
-    <div>
-      User profile page.
-    </div>
-  );
+const User = (props) => {
+  const user_id=Number(props.match.params.user);
+  const is_not_number=isNaN(user_id)
+  console.log(is_not_number)
+  if(!is_not_number){
+    return (
+        <div>
+          User profile page.
+        </div>
+    );
+  }
+  return <NotMatch/>
 };
 
 export default User;
